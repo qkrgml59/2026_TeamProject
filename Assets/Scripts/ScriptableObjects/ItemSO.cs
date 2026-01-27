14d29bb628129cd11399a7724f6035badefc7ecc
alphapo040511 <alphapo05@gmail.com>
2026-01-28 02:41:20 +0900

    [Test] Item 시스템 추가 진행중
    ItemSO 구조 고민중

---
+++ Assets/Scripts/ScriptableObjects/ItemSO.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Default_Item", menuName = "ItemSO/Default")]
+public abstract class ItemSO : MonoBehaviour
+{
+    [Header("아이템 이름")] public string itemName;
+    [Header("아이템 설명")] public string itemDescription;
+    [Header("아이템 아이콘")] public Sprite icon;
+    [Header("아이템 종류(재료, 완성, 유물 등")] public ItemType type;
+    [Header("추가 스텟")] public List<StatModifier> modifiers = new List<StatModifier>();
+}
