320f1fdfa338ae89e152f27c9e1e338eb5037bfc
qkrgml59 <qkrgml59@gmail.com>
2026-01-29 17:08:04 +0900

    [Test] UnitBase 수정중

---
+++ Assets/Scripts/Units System/IdleState.cs
@@ -0,0 +1,21 @@
+//using UnityEngine;
+
+//public class IdleState : IUnitState
+//{
+//    private UnitBase unit;
+
+//    public IdleState(UnitBase unit)
+//    {
+//        this.unit = unit;
+//    }
+
+//    public void StateEnter()
+//    {
+//        Debug.Log("진입");
+//    }
+
+//    public void StateUpdate()
+//    {
+//        var target = unit.FindTarget();
+//    }
+//}
--- Assets/Scripts/Units System/UnitBase.cs
+++ Assets/Scripts/Units System/UnitBase.cs
@@ -3,52 +3,30 @@ using UnityEngine;
 
 /// <summary>
 /// 모든 기물(Unit)이 상속받는 공통 베이스 클래스
+/// 상태 머신을 실행만 한다
+/// 판단은 각 State가 담당
 /// </summary>
 public abstract class UnitBase : MonoBehaviour
-{
+{  
     //현재 유닛이 수행중인 상태
     protected IUnitState currentState;
 
-    [Header("Team/HP")]
-    protected int teamId = 0;      //팀 이름 Ex) 아군 0, 적 1
-    public int TeamId => teamId;
-    public bool IsDead => maxHp <= 0;
-
-    //공통 스텟
-    [Header("Unit Stats")]
-    public int maxHp;                //최대 체력
-    public int currentHp;            //현재 체력
-    public float AttackDamage;       //공격력
-    public float AbilityPower;       //주문력
-    public float AttackSpeed;        //공격 속도
-    public int attackRange;          //공격 범위 (사거리)
-    public float moveSpeed;          //이동 속도
-    public float Defense;            //방어력
-    public float attackCooldown;     //공격 쿨타임
-
-    //현재 공격 또는 추적 중인 대상
-    protected UnitBase currentTarget;
-
-    //매 프레임 현재 상태의 Update 실행
     protected virtual void Update()
     {
+        //현재 상태의 Update실행
         currentState?.StateUpdate();
     }
 
-
-    //FixedUpdate 타이밍에 상태의 Fixedupdate실행
     protected virtual void FixedUpdate()
     {
+        //현재 상태의 FixedUpdate 실행
         currentState?.StateFixedUpdate();
     }
 
-
-    /// <summary>
-    /// 상태 전환 함수
-    /// - Exit -> Enter 순서 보장
-    /// - 외부에서는 반드시 이 함수를 통해 상태 변경
+    ///<summary>
+    ///상태 전환 함수
+    ///이 함수를 통해서만 상태 변경
     /// </summary>
-    
     public void ChangeState(IUnitState newState)
     {
         if (currentState == newState) return;
@@ -58,13 +36,35 @@ public abstract class UnitBase : MonoBehaviour
         currentState?.StateEnter();
     }
 
+    /// <summary>
+    /// 공통 데이터
+    /// </summary>
 
-    // ========
-    // 공통 행동
-    // ========
+    //스탯 묶음
+    protected StatSet stateSet;
 
- 
+    //스탯 원본 데이터
+    public UnitStatSO unitStatSO;
 
-  
+    //현재 공격 / 추적 중인 대상
+    protected UnitBase currentTarget;
+
+    protected virtual void Start()
+    {
+        stateSet = new StatSet(unitStatSO);         //SO기반으로 스탯 초기화
+    }
+
+    public void SetTarget(UnitBase target)
+    {
+        currentTarget = target;
+    }
+
+    public bool HasTarget()
+    {
+        return currentTarget != null;
+    }
+
+
+    //우선 수정 중이빈다.
 
 }
