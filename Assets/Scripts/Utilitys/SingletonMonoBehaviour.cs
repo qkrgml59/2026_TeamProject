c2c56e431573bd0299f1ee6dde4c043094fc6eb6
alphapo040511 <alphapo05@gmail.com>
2026-03-04 14:48:40 +0900

    [Utility] SingletoneMonoBehaviour 도구 추가

---
+++ Assets/Scripts/Utilitys/SingletonMonoBehaviour.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+namespace Tools
+{
+    /// <summary>
+    /// 모노 오브젝트를 싱글톤으로 설정 시켜주는 클래스입니다.
+    /// </summary>
+    public class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        [Header("씬 전환 유지 설정")]
+        [SerializeField] private bool useDontDestroyOnLoad = true;
+
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindAnyObjectByType<T>();
+                    if (_instance == null)
+                    {
+                        Debug.LogError($"현재 씬에 {typeof(T).Name}가 없습니다.");
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        private void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = this as T;
+
+                if (dontDestroyOnLoad)
+                    DontDestroyOnLoad(gameObject);
+            }
+            else if (_instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            if (_instance == this)
+                _instance = null;
+        }
+    }
+}
