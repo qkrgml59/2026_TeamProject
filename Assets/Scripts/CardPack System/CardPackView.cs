3dd6f70c5ac6b2c5ebc23760a904265ed98cd13e
alphapo040511 <alphapo05@gmail.com>
2026-04-17 13:22:18 +0900

    [Chore] update build version to 0.3.1

--- Assets/Scripts/CardPack System/CardPackView.cs
+++ Assets/Scripts/CardPack System/CardPackView.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
-using Unity.VisualScripting.ReorderableList;
 
 public class CardPackView : MonoBehaviour, IPointerClickHandler
 {
